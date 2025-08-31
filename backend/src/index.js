8357007d08c0262bca7d311f170e10404b4409a8
Razal Kabeer <razalbinkabeer01@gmail.com>
2025-08-31 14:30:51 +0530

    index file issues fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,19 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { connectDB } from "./lib/db.js";
-import { app, server } from "./lib/socket.js";
 
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
 dotenv.config();
-app;
 
-const PORT = process.env.PORT || 5001;
-
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
@@ -31,12 +31,12 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 server.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
